751047a34268143bb00fb40503621bd8612c0092
Lex <welioresse@gmail.com>
2024-05-05 22:59:31 +0700

    added hilt annotations

--- app/src/main/java/team/viceversa/template/MainActivity.kt
+++ app/src/main/java/team/viceversa/template/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import team.viceversa.template.ui.theme.TemplateTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
